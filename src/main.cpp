7a544a695d259ec6556dbcf20f6d73a642db1fb2
José Antonio Costa de Moya <joseantonio.costa250@gmail.com>
2020-03-22 20:07:32 +0100

    modificado para mostrar posición de los nodos

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,31 @@
  *********************************************************************/
 
 #include "ExampleApplication.h"
+#include <iostream>
 
 class SimpleExample : public ExampleApplication {
-  public : void createScene() {   
-    Ogre::Entity *ent = mSceneMgr->createEntity("Sinbad", "Sinbad.mesh");
-    mSceneMgr->setAmbientLight(Ogre::ColourValue(1, 1, 1));
-    mSceneMgr->getRootSceneNode()->attachObject(ent);
+  public :
+	void createScene() {
+		Ogre::Entity *ent = mSceneMgr->createEntity("MyEntity", "Sinbad.mesh");
+		mSceneMgr->setAmbientLight(Ogre::ColourValue(1, 1, 1));
+		Ogre::SceneNode* node = mSceneMgr->createSceneNode("Node1");
+		node->setPosition(10,10,0);
+		mSceneMgr->getRootSceneNode()->addChild(node);
+		node->attachObject(ent);
+
+		Ogre::Entity *ent2 = mSceneMgr->createEntity("MyEntity2", "Sinbad.mesh");
+		Ogre::SceneNode* node2 = node->createChildSceneNode("node2");
+		node2->setPosition(10,0,0);
+		node2->scale(2.0f, 2.0f, 2.0f);
+		node2->attachObject(ent2);
+		std::cout << "Posición Nodo 2:" << node2->_getDerivedPosition() << std::endl;
+
+
+		Ogre::Entity* ent3 = mSceneMgr->createEntity("MyEntity3", "Sinbad.mesh");
+		Ogre::SceneNode* node3 = node->createChildSceneNode("node3", Ogre::Vector3(20,0,0));
+		node3->scale(0.2f, 0.2f, 0.2f);
+		node3->attachObject(ent3);
+		std::cout << "Posición Nodo 3:" << node3->_getDerivedPosition() << std::endl;
   }
 };
 
